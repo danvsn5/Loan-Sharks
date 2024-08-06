ac2724550685780f617f10017873e71bbce3c0e1
axu732 <axu732@aucklanduni.ac.nz>
2024-08-06 17:05:34 +1200

    Created customer results controller

--- src/main/java/uoa/lavs/controllers/CustomerResultsController.java
+++ src/main/java/uoa/lavs/controllers/CustomerResultsController.java
@@ -1,3 +1,34 @@
 package uoa.lavs.controllers;
 
-public class CustomerResultsController {}
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+public class CustomerResultsController {
+  // Table will need to be populated with data from the database
+  @FXML private TableView<?> resultsTable;
+
+  @FXML private Button backButton;
+
+  @FXML private Button leaveButton;
+
+  // write for all fxml elements
+  @FXML
+  private void initialize() {
+    // Add initialization code here
+  }
+
+  @FXML
+  private void handleBackButtonAction() {
+    // Add back button action code here
+  }
+
+  @FXML
+  private void handleLeaveButtonAction() {
+    // Add leave button action code here
+  }
+
+  // Need to implement method when clicking on a row in the table
+  // to view the customer's details
+
+}
