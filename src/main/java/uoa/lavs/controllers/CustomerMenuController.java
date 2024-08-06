5906e2380f9f56b656b0c158cbdee2e86d28b43f
axu732 <axu732@aucklanduni.ac.nz>
2024-08-06 16:19:20 +1200

    Added more customer related controllers

---
+++ src/main/java/uoa/lavs/controllers/CustomerMenuController.java
@@ -0,0 +1,46 @@
+package uoa.lavs.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CustomerMenuController {
+  @FXML private Button addNewCustomerButton;
+
+  @FXML private Button findCustomerButton;
+
+  @FXML private Button loanButton;
+
+  @FXML private Button exitButton;
+
+  @FXML private Button backButton;
+
+  @FXML
+  private void initialize() {
+    // Add initialization code here
+  }
+
+  @FXML
+  private void handleAddNewCustomerButtonAction() {
+    // Add add new customer button action code here
+  }
+
+  @FXML
+  private void handleFindCustomerButtonAction() {
+    // Add find customer button action code here
+  }
+
+  @FXML
+  private void handleLoanButtonAction() {
+    // Add loan button action code here
+  }
+
+  @FXML
+  private void handleExitButtonAction() {
+    // Add exit button action code here
+  }
+
+  @FXML
+  private void handleBackButtonAction() {
+    // Add back button action code here
+  }
+}
---
+++ src/main/java/uoa/lavs/controllers/MainMenuController.java
@@ -0,0 +1,39 @@
+package uoa.lavs.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainMenuController {
+  @FXML private Button customerButton;
+
+  @FXML private Button loanButton;
+
+  @FXML private Button logOutButton;
+
+  @FXML private Button exitButton;
+
+  @FXML
+  private void initialize() {
+    // Add initialization code here
+  }
+
+  @FXML
+  private void handleCustomerButtonAction() {
+    // Add customer button action code here
+  }
+
+  @FXML
+  private void handleLoanButtonAction() {
+    // Add loan button action code here
+  }
+
+  @FXML
+  private void handleLogOutButtonAction() {
+    // Add log out button action code here
+  }
+
+  @FXML
+  private void handleExitButtonAction() {
+    // Add exit button action code here
+  }
+}
