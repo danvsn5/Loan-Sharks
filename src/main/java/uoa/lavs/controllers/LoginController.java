594229986fec6075dfa4f4f6c2b57275a81f714d
axu732 <axu732@aucklanduni.ac.nz>
2024-08-06 15:59:01 +1200

    Added LoginController and added JavaFX FXML dependancy

---
+++ src/main/java/uoa/lavs/controllers/LoginController.java
@@ -0,0 +1,31 @@
+package uoa.lavs.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+  @FXML private Button loginButton;
+
+  @FXML private Button exitButton;
+
+  @FXML private TextField usernameField;
+
+  @FXML private PasswordField passwordField;
+
+  @FXML
+  private void initialize() {
+    // Add initialization code here
+  }
+
+  @FXML
+  private void handleLoginButtonAction() {
+    // Add login button action code here
+  }
+
+  @FXML
+  private void handleExitButtonAction() {
+    // Add exit button action code here
+  }
+}
