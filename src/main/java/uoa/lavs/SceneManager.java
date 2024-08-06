7363fb947cc178518dbb22ffa6769b530694bfbc
axu732 <axu732@aucklanduni.ac.nz>
2024-08-06 21:18:35 +1200

    Added scene manager for controllers.

---
+++ src/main/java/uoa/lavs/SceneManager.java
@@ -0,0 +1,38 @@
+package uoa.lavs;
+
+import java.util.HashMap;
+import javafx.scene.Parent;
+
+public class SceneManager {
+  public enum AppUI {
+    CI_ADDRESS,
+    CI_CONFIRM,
+    CI_CONTACT,
+    CI_DETAILS,
+    CI_EMPLOYER,
+    CI_NOTES,
+    CUSTOMER_MENU,
+    CUSTOMER_RESULTS,
+    CUSTOMER_SEARCH,
+    CUSTOMER_SUMMARY,
+    LC_CUSTOMER_DETAILS,
+    LC_CUSTOMER_RESULTS,
+    LC_SEARCH,
+    LC_INPUT,
+    LOAN_MENU,
+    LOAN_SEARCH,
+    LOGIN,
+    MAIN_MENU,
+    PAYMENT_MENU,
+  }
+
+  private static HashMap<AppUI, Parent> sceneMap = new HashMap<AppUI, Parent>();
+
+  public static void addScene(AppUI scene, Parent parent) {
+    sceneMap.put(scene, parent);
+  }
+
+  public static Parent getScene(AppUI scene) {
+    return sceneMap.get(scene);
+  }
+}
