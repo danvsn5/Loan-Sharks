714016dafa1c6bd965b5d0c4b8ee99da1fd25ff6
Chulshin Kim <ckim349@aucklanduni.ac.nz>
2024-08-06 16:03:27 +1200

    feat: unit tests for classes that loan reference

---
+++ src/test/java/uoa/lavs/loan/LoanPaymentTest.java
@@ -0,0 +1,59 @@
+package uoa.lavs.loan;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LoanPaymentTest {
+  LoanPayment loanPayment;
+
+  @BeforeEach
+  public void setUp() {
+    loanPayment = new LoanPayment("monthly", "monthly", "1000", false);
+  }
+
+  @Test
+  public void testGetCompounding() {
+    assertEquals("monthly", loanPayment.getCompounding());
+  }
+
+  @Test
+  public void testSetCompounding() {
+    loanPayment.setCompounding("yearly");
+    assertEquals("yearly", loanPayment.getCompounding());
+  }
+
+  @Test
+  public void testGetPaymentFrequency() {
+    assertEquals("monthly", loanPayment.getPaymentFrequency());
+  }
+
+  @Test
+  public void testSetPaymentFrequency() {
+    loanPayment.setPaymentFrequency("yearly");
+    assertEquals("yearly", loanPayment.getPaymentFrequency());
+  }
+
+  @Test
+  public void testGetPaymentAmount() {
+    assertEquals("1000", loanPayment.getPaymentAmount());
+  }
+
+  @Test
+  public void testSetPaymentAmount() {
+    loanPayment.setPaymentAmount("2000");
+    assertEquals("2000", loanPayment.getPaymentAmount());
+  }
+
+  @Test
+  public void testGetInterestOnly() {
+    assertEquals(false, loanPayment.getInterestOnly());
+  }
+
+  @Test
+  public void testSetInterestOnly() {
+    loanPayment.setInterestOnly(true);
+    assertEquals(true, loanPayment.getInterestOnly());
+  }
+}
