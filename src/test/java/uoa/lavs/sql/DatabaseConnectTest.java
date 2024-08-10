c101cd36ddfa905d036e326060f747010859cbf5
Jamie Lee <126450738+eejl773@users.noreply.github.com>
2024-08-10 22:15:34 +1200

    feat: added AfterAll tearDown to delete test file after all tests have been run

--- src/test/java/uoa/lavs/sql/DatabaseConnectTest.java
+++ src/test/java/uoa/lavs/sql/DatabaseConnectTest.java
@@ -1,7 +1,8 @@
 package uoa.lavs.sql;
 
+import java.io.File;
 import java.sql.Connection;
-
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 public class DatabaseConnectTest {
   DatabaseConnection conn;
   private static final String DB_URL = "jdbc:sqlite:src/test/java/uoa/lavs/sql/test.db";
+  private static File dbFile = new File("src/test/java/uoa/lavs/sql/test.db");
 
   @BeforeEach
   public void setUp() {
@@ -20,5 +22,12 @@ public class DatabaseConnectTest {
     Connection connection = conn.connect();
     Assertions.assertNotNull(connection);
     conn.close(connection);
+  }
+
+  @AfterAll
+  public static void tearDown() {
+      if (!dbFile.delete()) {
+          throw new RuntimeException("Failed to delete test database file: " + dbFile.getAbsolutePath());
+      }
   }
 }
