e1eeb454ba8af1ed8e3fe405d1b4ac1382c07de9
danvsn5 <danvsn5@gmail.com>
2024-08-12 20:19:45 +1200

    feat: implemented search results tab. NOTE: this functionality will operate by converting the text labels to their required values (from the user's search), either by setting their values to a name | Id, or by setting them to empty strings, indicating the max amount of results are shown above

--- src/main/java/uoa/lavs/Main.java
+++ src/main/java/uoa/lavs/Main.java
@@ -86,7 +86,7 @@ public class Main extends Application {
   @Override
   public void start(final Stage stage) throws IOException {
 
-    SceneManager.addScene(AppUI.LOGIN, loadFxml("loan_search"));
+    SceneManager.addScene(AppUI.LOGIN, loadFxml("customer_search_results"));
     System.out.println("Loading login scene");
     scene = new Scene(SceneManager.getScene(AppUI.LOGIN), 1280, 720);
 
