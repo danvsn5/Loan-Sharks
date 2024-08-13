1ff75ceb1d0ae8bcb30997143d5c3cd84ec5f369
Chulshin Kim <ckim349@aucklanduni.ac.nz>
2024-08-13 16:49:04 +1200

    feat: singleton design pattern for customer and loan objects

---
+++ src/main/java/uoa/lavs/customer/IndividualCustomerSingleton.java
@@ -0,0 +1,23 @@
+package uoa.lavs.customer;
+
+public class IndividualCustomerSingleton {
+  private static IndividualCustomer instance;
+
+  private IndividualCustomerSingleton() {}
+
+  public static IndividualCustomer getInstance() {
+    if (instance == null) {
+      // Initialize with placeholder values
+      instance = new IndividualCustomer("", "", "", "", "", null, "", "", null, null, null, null);
+    }
+    return instance;
+  }
+
+  public static void setInstance(IndividualCustomer customer) {
+    instance = customer;
+  }
+
+  public static void resetInstance() {
+    instance = null;
+  }
+}
---
+++ src/test/java/uoa/lavs/loan/PersonalLoanSingletonTest.java
@@ -0,0 +1,64 @@
+package uoa.lavs.loan;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PersonalLoanSingletonTest {
+  private PersonalLoan newLoan;
+  private ArrayList<String> coborrowers;
+  private Double principal;
+  private Double rate;
+  private LoanDuration duration;
+  private LoanPayment payment;
+  private PersonalLoan loan;
+
+  @BeforeEach
+  public void setUp() {
+    PersonalLoanSingleton.resetInstance();
+
+    coborrowers = new ArrayList<>();
+    coborrowers.add("000002");
+    coborrowers.add("000003");
+    principal = 1000.00;
+    rate = 0.1;
+    duration = new LoanDuration(LocalDate.of(2024, 6, 8), 1, 12);
+    payment = new LoanPayment("monthly", "100.0", "100.0", false);
+
+    newLoan = new PersonalLoan(1, "000001", coborrowers, principal, rate, duration, payment);
+  }
+
+  @Test
+  public void testGetInstance() {
+    loan = PersonalLoanSingleton.getInstance();
+    assertNotNull(loan);
+  }
+
+  @Test
+  public void testSetInstance() {
+    PersonalLoanSingleton.setInstance(newLoan);
+    loan = PersonalLoanSingleton.getInstance();
+
+    assertSame(newLoan, loan);
+  }
+
+  @Test
+  public void testResetInstance() {
+    PersonalLoanSingleton.setInstance(newLoan);
+    loan = PersonalLoanSingleton.getInstance();
+    PersonalLoanSingleton.resetInstance();
+    PersonalLoan loan2 = PersonalLoanSingleton.getInstance();
+
+    assertNotSame(loan, loan2);
+  }
+
+  @Test
+  public void testSingleUniqueInstance() {
+    assertSame(PersonalLoanSingleton.getInstance(), PersonalLoanSingleton.getInstance());
+  }
+}
