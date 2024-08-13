fbc5c32af7197e672313d9d2a1fc79966942e851
danvsn5 <danvsn5@gmail.com>
2024-08-13 12:31:21 +1200

    feat: added loan summary scene

--- src/main/java/uoa/lavs/Main.java
+++ src/main/java/uoa/lavs/Main.java
@@ -88,7 +88,7 @@ public class Main extends Application {
     // activates antialiasing for text in application !IMPORTANT
     System.setProperty("prism.lcdtext", "false");
 
-    SceneManager.addScene(AppUI.LOGIN, loadFxml("customer_contact_summary"));
+    SceneManager.addScene(AppUI.LOGIN, loadFxml("primary_address_details_summary"));
     System.out.println("Loading login scene");
     scene = new Scene(SceneManager.getScene(AppUI.LOGIN), 1280, 720);
     // imports main index.css file
