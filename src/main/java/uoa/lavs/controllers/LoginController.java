1c2642686cd91d09e30cf02e94fa0829aff367e9
axu732 <axu732@aucklanduni.ac.nz>
2024-08-15 19:27:31 +1200

    Login screen change implemented

--- src/main/java/uoa/lavs/controllers/LoginController.java
+++ src/main/java/uoa/lavs/controllers/LoginController.java
@@ -4,11 +4,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import uoa.lavs.Main;
+import uoa.lavs.SceneManager.AppUI;
 
 public class LoginController {
   @FXML private Button loginButton;
-
-  @FXML private Button exitButton;
 
   @FXML private TextField usernameField;
 
@@ -22,10 +23,18 @@ public class LoginController {
   @FXML
   private void handleLoginButtonAction() {
     // Add login button action code here
+    if (usernameField.getText().equals("Moana") && passwordField.getText().equals("Password")) {
+      System.out.println("Login successful");
+      Main.setUi(AppUI.MAIN_MENU);
+    } else {
+      System.out.println("Login failed");
+    }
   }
 
   @FXML
-  private void handleExitButtonAction() {
-    // Add exit button action code here
+  private void onEnterPressed(KeyEvent event) {
+    if (event.getCode().toString().equals("ENTER")) {
+      handleLoginButtonAction();
+    }
   }
 }
