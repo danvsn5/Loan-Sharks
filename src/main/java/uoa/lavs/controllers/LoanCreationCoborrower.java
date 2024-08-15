19202d54e4b22cd0846b8ef1a646a9ff463f1347
axu732 <axu732@aucklanduni.ac.nz>
2024-08-15 17:18:45 +1200

    Redid controllers to better fit FXML scenes. Connected more FXMLs to controllers

--- src/main/java/uoa/lavs/controllers/LoanCreationCoborrower.java
+++ src/main/java/uoa/lavs/controllers/LoanCreationCoborrower.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 
-public class LoanCreationCustomerDetailsController {
+public class LoanCreationCoborrower {
   @FXML private Label customerTitleLabel;
   @FXML private Label customerFirstNameLabel;
   @FXML private Label customerMiddleNameLabel;
